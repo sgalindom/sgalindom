e6f370eda4973f3d3b0f1c4d5315b20995a14fa9
sgalindom <sgalindom2612@gmail.com>
2024-05-22 22:08:02 -0500

    MASTER

--- src/components/MainPanel.js
+++ src/components/MainPanel.js
@@ -12,7 +12,9 @@ const MainPanel = ({ navigation }) => {
   const fadeAnim = useRef(new Animated.Value(1)).current;
   const [foodProducts, setFoodProducts] = useState([]);
   const [accessoryProducts, setAccessoryProducts] = useState([]);
-  const [allProductProducts, setProductProducts] = useState([]); // Corregido
+  const [allProductProducts, setProductProducts] = useState([]);
+  const [veterinarias, setVeterinarias] = useState([]);
+  const [guarderiasAdiestramiento, setGuarderiasAndAdiestramiento] = useState([]);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -102,41 +104,42 @@ const MainPanel = ({ navigation }) => {
         Animated.sequence([
           Animated.timing(fadeAnim, {
             toValue: 0,
-            duration: 500,
+            duration: 500, // Disminuí la duración de la animación de desvanecimiento
             useNativeDriver: true,
           }),
           Animated.timing(slideAnim, {
-            toValue: -300,
-            duration: 500,
+            toValue: -200,
+            duration: 1500, // Mantuve la duración del desplazamiento
             useNativeDriver: true,
             easing: Easing.inOut(Easing.ease),
           }),
           Animated.timing(slideAnim, {
-            toValue: 300,
+            toValue: 200,
             duration: 0,
             useNativeDriver: true,
           }),
           Animated.timing(slideAnim, {
             toValue: 0,
-            duration: 500,
+            duration: 1500, // Mantuve la duración del desplazamiento
             useNativeDriver: true,
             easing: Easing.inOut(Easing.ease),
           }),
           Animated.timing(fadeAnim, {
             toValue: 1,
-            duration: 500,
+            duration: 500, // Disminuí la duración de la animación de desvanecimiento
             useNativeDriver: true,
           }),
         ]).start();
-
+  
         setTimeout(() => {
           setCurrentServiceIndex(prevIndex => (prevIndex + 1) % services.length);
-        }, 500); // Change the service index after the fade-out animation
-      }, 4000);
-
+        }, 2000); // Disminuí el tiempo de espera antes de cambiar al siguiente servicio a 2 segundos
+      }, 8000); // Mantuve el tiempo total entre cada cambio de servicio a 8 segundos
+  
       return () => clearInterval(intervalId);
     }
   }, [slideAnim, fadeAnim, services.length]);
+  
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -146,86 +149,134 @@ const MainPanel = ({ navigation }) => {
             .collection('Veterinarias')
             .doc(vetId.toString())
             .collection('Comida');
-
+  
           const snapshot = await vetFoodProductsRef.get();
-
+  
           return snapshot.docs.map(doc => ({
             id: doc.id,
             vetId: vetId,
             ...doc.data(),
           }));
         });
-
+  
         const foodProductsData = await Promise.all(foodProductsRefs);
         const allFoodProducts = foodProductsData.reduce((acc, val) => acc.concat(val), []);
         setFoodProducts(allFoodProducts);
-
+  
         const accessoryProductsRefs = [1, 2, 3].map(async (vetId) => {
           const vetAccessoryProductsRef = firestore()
             .collection('Veterinarias')
             .doc(vetId.toString())
             .collection('Accesorios');
-
+  
           const snapshot = await vetAccessoryProductsRef.get();
-
+  
           return snapshot.docs.map(doc => ({
             id: doc.id,
-            vetId: vetId, ...doc.data(),
+            vetId: vetId,
+            ...doc.data(),
           }));
         });
-
+  
         const accessoryProductsData = await Promise.all(accessoryProductsRefs);
         const allAccessoryProducts = accessoryProductsData.reduce((acc, val) => acc.concat(val), []);
         setAccessoryProducts(allAccessoryProducts);
-
+  
         const productProductsRefs = [1, 2, 3].map(async (vetId) => {
           const vetProductProductsRef = firestore()
             .collection('Veterinarias')
             .doc(vetId.toString())
             .collection('Productos');
-
+  
           const snapshot = await vetProductProductsRef.get();
-
+  
           return snapshot.docs.map(doc => ({
             id: doc.id,
-            vetId: vetId, ...doc.data(),
+            vetId: vetId,
+            ...doc.data(),
           }));
         });
-
+  
         const productProductsData = await Promise.all(productProductsRefs);
         const allProductProducts = productProductsData.reduce((acc, val) => acc.concat(val), []);
         setProductProducts(allProductProducts);
+  
+        const veterinariasRef = firestore().collection('Veterinarias');
+        const veterinariasSnapshot = await veterinariasRef.get();
+  
+        const allVeterinarias = veterinariasSnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+  
+        setVeterinarias(allVeterinarias);
       } catch (error) {
-        console.error('Error fetching products:', error);
+        console.error('Error fetching products', error);
       }
     };
-
+  
+    const fetchGuarderiasAndAdiestramiento = async () => {
+      try {
+        const guarderiasAndAdiestramientoRefs = [1, 2, 3].map(async (id) => {
+          const guarderiasAndAdiestramientoRef = firestore()
+            .collection('Guarderiasyadiestramiento')
+            .doc(id.toString());
+  
+          const snapshot = await guarderiasAndAdiestramientoRef.get();
+  
+          return snapshot.exists ? { id: snapshot.id, ...snapshot.data() } : null;
+        });
+  
+        const guarderiasAndAdiestramientoData = await Promise.all(guarderiasAndAdiestramientoRefs);
+  
+        // Filtrar elementos nulos (documentos que no existen)
+        const filteredData = guarderiasAndAdiestramientoData.filter(item => item !== null);
+  
+        setGuarderiasAndAdiestramiento(filteredData);
+      } catch (error) {
+        console.error('Error fetching guarderias and adiestramiento data:', error);
+      }
+    };
+  
     fetchProducts();
+    fetchGuarderiasAndAdiestramiento();
   }, []);
+  
 
   const handleProductPress = (productId, vetId, productType) => {
     let route = '';
     switch (productType) {
       case 'comida':
-        route = vetId === 1 ? 'vet1comida' : 'vet3comida';
+        route = vetId === 1 ? 'vet1comida' : vetId === 2 ? 'vet2comida' : 'vet3comida';
         break;
       case 'accesorios':
-        route = vetId === 1 ? 'vet1accesorios' : 'vet3accesorios';
+        route = vetId === 1 ? 'vet1accesorios' : vetId === 2 ? 'vet2accesorios' : 'vet3accesorios';
         break;
       case 'productos':
-        route = vetId === 1 ? 'vet1productos' : 'vet3productos';
+        route = vetId === 1 ? 'vet1juguetes' : vetId === 2 ? 'vet2productos' : 'vet3productos';
         break;
       default:
         console.error('Tipo de producto no reconocido');
         break;
     }
-  
+
     if (route !== '') {
       navigation.navigate(route);
     } else {
       console.error('Ruta de navegación no válida');
     }
   };
+
+  const handleVetPress = (vetId) => {
+    const route = `vet${vetId}`;
+    navigation.navigate(route);
+  };
+
+  const handleGuarderiaAdiestramientoPress = (id) => {
+    const route = `Adieguar${id}`;
+    navigation.navigate(route);
+  };
+  
 
   const slide = slideAnim.interpolate({
     inputRange: [-300, 0, 300],
@@ -258,41 +309,80 @@ const MainPanel = ({ navigation }) => {
       </Animated.View>
           </View>
         )}
-
+  
         <View style={styles.tipCard}>
           <Text style={styles.tipText}>{tips[currentServiceIndex % tips.length].text}</Text>
         </View>
-
+  
         {/* Productos de comida */}
         <View style={styles.productContainer}>
           <Text style={styles.productTitle}>Productos de Comida</Text>
           <ScrollView horizontal={true}>
             {foodProducts.map(product => (
-              <TouchableOpacity key={product.id} style={styles.productCard} onPress={() => handleProductPress(product.id, product.vetId)}>
+              <TouchableOpacity key={product.id} style={styles.productCard} onPress={() => handleProductPress(product.id, product.vetId, 'comida')}>
                 <Image source={{ uri: product.Foto }} style={styles.productImage} />
                 <Text style={styles.productName}>{product.Nombre}</Text>
-                <Text style={styles.productPrice}>{product.Precio}</Text>
+                <Text style={styles.productPrice}>${product.Precio}</Text>
               </TouchableOpacity>
             ))}
           </ScrollView>
         </View>
-
+  
         {/* Productos de accesorios */}
         <View style={styles.productContainer}>
           <Text style={styles.productTitle}>Productos de Accesorios</Text>
           <ScrollView horizontal={true}>
             {accessoryProducts.map(product => (
-              <TouchableOpacity key={product.id} style={styles.productCard} onPress={() => handleProductPress(product.id, product.vetId)}>
+              <TouchableOpacity key={product.id} style={styles.productCard} onPress={() => handleProductPress(product.id, product.vetId, 'accesorios')}>
                 <Image source={{ uri: product.Foto }} style={styles.productImage} />
                 <Text style={styles.productName}>{product.Nombre}</Text>
-                <Text style={styles.productPrice}>{product.Precio}</Text>
+                <Text style={styles.productPrice}>${product.Precio}</Text>
               </TouchableOpacity>
             ))}
           </ScrollView>
         </View>
-
+  
+        {/* Productos */}
+        <View style={styles.productContainer}>
+          <Text style={styles.productTitle}>Productos</Text>
+          <ScrollView horizontal={true}>
+            {allProductProducts.map(product => (
+              <TouchableOpacity key={product.id} style={styles.productCard} onPress={() => handleProductPress(product.id, product.vetId, 'productos')}>
+                <Image source={{ uri: product.Foto }} style={styles.productImage} />
+                <Text style={styles.productName}>{product.Nombre}</Text>
+                <Text style={styles.productPrice}>${product.Precio}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+  
+        {/* Veterinarias */}
+        <View style={styles.veterinariasContainer}>
+          <Text style={styles.veterinariasTitle}>Veterinarias</Text>
+          <ScrollView horizontal={true}>
+            {veterinarias.map(vet => (
+              <TouchableOpacity key={vet.id} style={styles.veterinariaCard} onPress={() => handleVetPress(vet.id)}>
+                <Image source={{ uri: vet.Foto }} style={styles.veterinariaImage} />
+                <Text style={styles.veterinariaName}>{vet.Nombre}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+  
+        {/* Guarderías y Adiestramiento */}
+        <View style={styles.guarderiasAdiestramientoContainer}>
+          <Text style={styles.guarderiasAdiestramientoTitle}>Guarderías y Adiestramiento</Text>
+          <ScrollView horizontal={true}>
+            {guarderiasAdiestramiento.map(guarderia => (
+              <TouchableOpacity key={guarderia.id} style={styles.guarderiaAdiestramientoCard} onPress={() => handleGuarderiaAdiestramientoPress(guarderia.id)}>
+                <Image source={{ uri: guarderia.Foto }} style={styles.guarderiaAdiestramientoImage} />
+                <Text style={styles.guarderiaAdiestramientoName}>{guarderia.Nombre}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
       </ScrollView>
-
+  
       <TouchableOpacity
         style={styles.profileBar}
         onPress={() => navigation.navigate('MiPerfil')}
@@ -366,56 +456,108 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333',
   },
-  profileBar: {
-    backgroundColor: '#333',
-    padding: 10,
-    alignItems: 'center',
-  },
-  profileText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
   productContainer: {
-    marginTop: 20,
-    marginLeft: 20,
+    marginVertical: 10,
   },
   productTitle: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 10,
+    paddingHorizontal: 10,
+    marginBottom: 5,
   },
   productCard: {
-    marginRight: 10,
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-    borderRadius: 10,
-    padding: 10,
     width: 150,
-    height: 200,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
-    elevation: 3,
+    marginHorizontal: 5,
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginBottom: 10,
   },
   productImage: {
-    width: 120,
-    height: 120,
+    width: 150,
+    height: 100,
     resizeMode: 'cover',
-    borderRadius: 10,
-    marginBottom: 10,
   },
   productName: {
     fontSize: 14,
     fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 5,
+    marginTop: 5,
   },
   productPrice: {
     fontSize: 12,
-    color: '#888',
-    textAlign: 'center',
+    color: '#666666',
+    marginBottom: 5,
+  },
+  veterinariasContainer: {
+    marginVertical: 10,
+  },
+  veterinariasTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+    marginBottom: 5,
+  },
+  veterinariaCard: {
+    width: 150,
+    marginHorizontal: 5,
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  veterinariaImage: {
+    width: 150,
+    height: 100,
+    resizeMode: 'cover',
+  },
+  veterinariaName: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+  guarderiasAdiestramientoContainer: {
+    marginVertical: 10,
+  },
+  guarderiasAdiestramientoTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+    marginBottom: 5,
+  },
+  guarderiaAdiestramientoCard: {
+    width: 150,
+    marginHorizontal: 5,
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  guarderiaAdiestramientoImage: {
+    width: 150,
+    height: 100,
+    resizeMode: 'cover',
+  },
+  guarderiaAdiestramientoName: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+  profileBar: {
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
+    backgroundColor: '#0066cc',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 30,
+  },
+  profileText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
